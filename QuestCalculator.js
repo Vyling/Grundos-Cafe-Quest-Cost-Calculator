bf408c2fb2fd668917e245fb86823725bf8580f8
Kyu <komaron@sauercloud.net>
2023-04-14 12:12:25 +0200

    change map to array, add deadline check, remove onclick check for item name in quest items to display on Wiz

--- QuestCalculator.js
+++ QuestCalculator.js
@@ -2,7 +2,7 @@
 // @name         Grundos Cafe Quest Cost Calculator
 // @namespace    https://www.grundos.cafe
 // @namespace    https://grundos.cafe
-// @version      0.1
+// @version      0.3
 // @description  Calculates the cost of the Grundos Cafe Quest items as the user searches for them on the Shop Wiz. Displays the info in a table above the Shop Wiz Search. Does not include Main Shops. Only Shop Wiz.
 // @author       Dark_Kyuubi
 // @match        https://www.grundos.cafe/market/wizard*
@@ -22,6 +22,14 @@
 // @license      MIT
 // ==/UserScript==
 
+class QuestItems {
+    constructor(questGiver, items, deadline) {
+        this.questGiver = questGiver;
+        this.items = items;
+        this.deadline = deadline;
+    }
+}
+
 class Item {
     constructor(name, cost) {
         this.name = name;
@@ -30,32 +38,47 @@ class Item {
 }
 
 //Map serialization and proper Map checking is pain
-let quests = JSON.parse(GM_getValue('quests'))?.reduce((m, [key, val]) => m.set(key, val), new Map()) instanceof Map ? JSON.parse(GM_getValue('quests')).reduce((m, [key, val]) => m.set(key, val), new Map()) : new Map();
+let quests = [];
 let questsGrid = document.createElement('div');
+
+const styles = `
+    .quest-item,.quest-giver,.final-cost { 
+        width: 100%;
+        height: 100%;
+        border: 1px solid black;
+`
+let styleSheet = document.createElement("style");
 
 (function () {
     'use strict';
+    
+    initializeQuests();
+
     if (!window.location.href.includes("wizard")) {
         let questGiver = determineQuestGiver();
         if (window.location.href.includes("complete")) {
-            quests.delete(questGiver);
-            quests.size == 0 ? GM_deleteValue('quests') : updateStoredQuests();
+            if (!document.querySelector("strong.red")) {
+                quests = quests.filter(q => q.questGiver !== questGiver);
+                quests.length == 0 ? GM_deleteValue('quests') : updateStoredQuests();
+            }
         } else {
             getNewQuestItems(questGiver);
         }
-        document.querySelector('.searchhelp>a[href*="wizard"]').onclick = function () {
-            GM_setValue('fromQuest', true);
-        }
     } else {
-        if (quests.size > 0 && GM_getValue('fromQuest', false)) {
-            showCalculatedQuestsSumOnWiz();
+        let shopWizResult = document.querySelector('.sw_results');
+        let swItem = document.querySelector('.nomargin>strong')?.innerHTML.substring(18, undefined).trim();
+        if (quests.length > 0 && shopWizResult && isQuestItem(swItem)) {
+            pushStyle();
+            showCalculatedQuestsSumOnWiz(shopWizResult, swItem);
         }
-        //I just need someplace to reset the value once the Wiz had been visited
-        GM_setValue('fromQuest', false);
     }
 
-    function showCalculatedQuestsSumOnWiz() {
-        collectShopWizPrice();
+    function isQuestItem(swItem) {
+        return quests.some(q => q.items.some(i => i.name === swItem));
+    }
+
+    function showCalculatedQuestsSumOnWiz(shopWizResult, swItem) {
+        collectShopWizPrice(swItem);
         questsGrid.setAttribute('style', getGridStyle());
         createQuestGiverHeaders();
         createQuestItemRows();
@@ -63,22 +86,20 @@ let questsGrid = document.createElement('div');
         //to find it easier in the DOM for debugging purposes
         questsGrid.className = 'quest-grid';
         //try not to insert on main Shop Wiz page, only on result page
-        document.getElementsByTagName('main')[0]?.insertBefore(questsGrid, document.querySelector('.sw_results'));
+        document.getElementsByTagName('main')[0]?.insertBefore(questsGrid, shopWizResult);
     }
 
     function getGridStyle() {
-        let gridStyle = 'display: grid; border: 1px solid black;';
+        let gridStyle = 'display: grid; border: 1px solid black;text-align: center;';
         //columns
-        gridStyle += 'grid-template-columns: repeat(' + quests.size + ', 1fr);';
-        //rows
-        gridStyle += 'grid-template-rows: repeat(auto-fill, 1fr);';
+        gridStyle += 'grid-template-columns: repeat(' + quests.length + ', 1fr);';
         return gridStyle;
     }
 
     function createQuestItemRows() {
         for (let i = 0; i < 4; i++) {
-            for (const key of quests.keys()) {
-                let itemName = quests.get(key)[i]?.name;
+            for (const quest of quests) {
+                let itemName = quest.items[i]?.name;
                 if (!itemName) {
                     itemName = "-";
                 }
@@ -87,8 +108,7 @@ let questsGrid = document.createElement('div');
         }
     }
 
-    function collectShopWizPrice() {
-        let swItem = document.querySelector('.nomargin>strong')?.innerHTML.substring(18, undefined).trim();
+    function collectShopWizPrice(swItem) {
         let swPrice = parseInt(document.querySelector('.sw_results>.data>strong')?.innerHTML.match(/\d+/g).join(''));
         if (swItem && swPrice) {
             updatePriceForQuestItems(swItem, swPrice);
@@ -97,18 +117,60 @@ let questsGrid = document.createElement('div');
     }
 
     function getNewQuestItems(questGiver) {
-        let questItemsElements = document.querySelectorAll('.shop-item');
-        if (questItemsElements) {
+        //when you accept a quest, they are listed in .shop-item, when you return to a quest, they are listed as .quest-item... that's annoying.
+        let questItemsElements = document.querySelectorAll('.shop-item').length > 0 ? document.querySelectorAll('.shop-item') : document.querySelectorAll('.quest-item');
+        if (questItemsElements.length > 0) {
             let questItems = getQuestItems(questItemsElements);
-            quests.set(questGiver, questItems);
+            let deadline = getDeadline();
+            let existingQuestIndex = quests.findIndex(q => q.questGiver === questGiver);
+            if (existingQuestIndex !== -1) {
+                quests[existingQuestIndex] = new QuestItems(questGiver, questItems, deadline);
+            } else {
+                quests.push(new QuestItems(questGiver, questItems, deadline));
+            }
             updateStoredQuests();
         }
     }
 })();
 
+/**
+ * Provides fallback options for previous versions of the script.
+ * Will delete quests that ran out.
+ */
+function initializeQuests() {
+    const storedQuests = GM_getValue('quests') ? JSON.parse(GM_getValue('quests')) : [];
+    if (storedQuests && !storedQuests instanceof Array) {
+        const parsedStoredQuests = JSON.parse(storedQuests)?.reduce((m, [key, val]) => m.set(key, val), new Map());
+        if (parsedStoredQuests instanceof Map) {
+            //delete old incompatible storage values
+            GM_deleteValue('quests');
+        }
+    } else if (storedQuests) {
+        quests = storedQuests.filter(questIem => questIem.deadline > Date.now());
+    }
+}
+
+function getDeadline() {
+    var xpath = "//span[contains(text(),'minutes')]";
+    const questCountDownAfterAccepting = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    var matchingElement = questCountDownAfterAccepting ? questCountDownAfterAccepting : document.querySelector('.taelia_countdown');
+    if (matchingElement) {
+        const matchingElementText = matchingElement.innerText;
+        let hours = matchingElementText.substring(matchingElementText.indexOf('hrs') - 2, matchingElementText.indexOf('hrs') - 1);
+        let minutes = matchingElementText.substring(matchingElementText.indexOf('minutes') - 3, matchingElementText.indexOf('minutes') - 1);
+        let seconds = matchingElementText.substring(matchingElementText.indexOf('seconds') - 3, matchingElementText.indexOf('seconds') - 1);
+        return new Date(Date.now() + (hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds * 1000)).getTime();
+    }
+}
+
+function pushStyle() {
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+}
+
 function createQuestSumRow() {
-    for (const key of quests.keys()) {
-        let items = quests.get(key);
+    for (const quest of quests) {
+        let items = quest.items;
         let sum = 0;
         for (const item of items) {
             sum += item.cost;
@@ -118,25 +180,23 @@ function createQuestSumRow() {
 }
 
 function createQuestGiverHeaders() {
-    for (const key of quests.keys()) {
-        console.log("adding " + key + " to header row");
-        questsGrid.innerHTML += '<div class="quest-giver">' + key + '</div>';
+    for (const quest of quests) {
+        questsGrid.innerHTML += '<div class="quest-giver">' + quest.questGiver + '</div>';
     };
 }
 
 function updateStoredQuests() {
-    GM_setValue('quests', JSON.stringify(Array.from(quests)));
+    GM_setValue('quests', JSON.stringify(quests));
 }
 
 function updatePriceForQuestItems(swItem, swPrice) {
-    for (const arrayOfValues of quests.values()) {
-        for (const value of arrayOfValues) {
-            if (value.name === swItem) {
-                console.log("udpating: ", value.name);
-                value.cost = swPrice;
+    quests.forEach(quest => {
+        quest.items.forEach(item => {
+            if (item.name === swItem) {
+                item.cost = swPrice;
             }
-        }
-    };
+        })
+    });
 }
 
 function getQuestItems(questItemsElements) {
